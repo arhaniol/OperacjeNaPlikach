1765647c7f83908d8ae77caaf00888fe98fcff57
arhaniol <arhaniol@op.pl>
2020-01-08 20:38:46 +0100

    task 2 solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,47 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.NoSuchFileException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] arg) {
         System.out.println("Task 1:");
         task1();
+        System.out.println("Task 2:");
+        task2();
+    }
+
+    private static void task2() {
+        BufferedReader fileReader = null;
+        String filePath,
+                line;
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+
+        System.out.print("Podaj scieżkę pliku do odczytu: ");
+        filePath = input.nextLine();
+
+        try {
+            fileReader = new BufferedReader(new FileReader(filePath));
+            do {
+                line = fileReader.readLine();
+                if (line != null) {
+                    count++;
+                }
+            } while (line != null);
+            System.out.println("Przyczytano " + count + " linijek tekstu.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Nieprawidłowa ścieżka :(");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private static void task1() {
@@ -27,6 +63,8 @@ public class Main {
                     fileWriter.write(line + System.lineSeparator());
                 }
             } while (isEnd);
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie wprowadzono scieżki :(");
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
